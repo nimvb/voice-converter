f2038f43c43243f91d87b4f2dbe6dac56a09a33e
Nima Afzal <nima.afzal.ahangaran@gmail.com>
2023-07-07 18:55:26 +0330

    feat(doc): integrate controller methods with the required security requirements

--- src/main/java/com/nimvb/app/voice/converter/controller/HelloWorldController.java
+++ src/main/java/com/nimvb/app/voice/converter/controller/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.nimvb.app.voice.converter.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/tests")
+@SecurityRequirement(name = "bearer-authentication")
 public class HelloWorldController {
 
     @GetMapping
